baa040914073ca1ab3d3a97ec149217b9a16a5dc
Jan Duplinszki <duplinszkijan@gmail.com>
2019-01-18 16:33:02 +0000

    Updates

---
+++ precache-manifest.801799b4a6bc27a8cfd21d6e7cfc3caa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb3161904e04f35fb2b3e792d7395c3c",
+    "url": "/PersonalWebsite/static/media/veg.eb316190.png"
+  },
+  {
+    "revision": "81c6cd6d3a8cb8b8b1da",
+    "url": "/PersonalWebsite/static/css/main.bd6cf7fa.chunk.css"
+  },
+  {
+    "revision": "41702c6cd2ca3e75273f",
+    "url": "/PersonalWebsite/static/js/1.41702c6c.chunk.js"
+  },
+  {
+    "revision": "2997cd6cd44eaed4d4af",
+    "url": "/PersonalWebsite/static/js/runtime~main.2997cd6c.js"
+  },
+  {
+    "revision": "009c04f48fb18c7601d5bfd9845eeaab",
+    "url": "/PersonalWebsite/static/media/todo.009c04f4.png"
+  },
+  {
+    "revision": "81c6cd6d3a8cb8b8b1da",
+    "url": "/PersonalWebsite/static/js/main.81c6cd6d.chunk.js"
+  },
+  {
+    "revision": "e63d6ec0e433182a4f90895765e584ff",
+    "url": "/PersonalWebsite/static/media/weather.e63d6ec0.png"
+  },
+  {
+    "revision": "1ea90675a47062db0d9689e32a2a875c",
+    "url": "/PersonalWebsite/static/media/workout.1ea90675.png"
+  },
+  {
+    "revision": "7c931a9aefbae38a44e938aba72ff78c",
+    "url": "/PersonalWebsite/static/media/logo.7c931a9a.png"
+  },
+  {
+    "revision": "53c2b0f975f82a01e161c68d050c28d5",
+    "url": "/PersonalWebsite/static/media/bg2.53c2b0f9.svg"
+  },
+  {
+    "revision": "c6ae4f87726233423304a50388cd0928",
+    "url": "/PersonalWebsite/index.html"
+  }
+];
