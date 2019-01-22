c50a4e458d9ad9357b5e1ea53064a1b75ad20032
Jan Duplinszki <duplinszkijan@gmail.com>
2019-01-22 22:34:50 +0000

    Update index.js

--- src/animation/index.js
+++ src/animation/index.js
@@ -4,7 +4,6 @@ export const AnimatedAppContainer = posed.div({
   enter: {opacity: 1, delay: 200, beforeChildren: true},
   exit: {opacity: 0}
 });
-
 export const AnimatedContainer = posed.div({
   enter: {staggerChildren: 50, beforeChildren: true}
 });
@@ -16,7 +15,6 @@ export const AnimatedTitle = posed.div({
   enter: {y: 0, opacity: 1},
   exit: {y: -50, opacity: 0}
 });
-
 export const AnimatedIcon = posed.div({
   enter: {y: 0, opacity: 1},
   exit: {y: 50, opacity: 0}
