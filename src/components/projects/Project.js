1a727a9636613a227fc9f1b1a984b9b4c54f9681
Jan Duplinszki <duplinszkijan@gmail.com>
2019-01-26 11:08:43 +0000

    Update Project.js

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import {Title} from "../About";
 import ProjectCard from "./ProjectCard";
-
 import {AnimatedTitle} from "../../animation";
 
 const Project = () => (
